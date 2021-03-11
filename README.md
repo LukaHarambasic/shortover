4968a96b726500822fcec848f19170e3db1fa412
Luka Harambasic <luka@harambasic.de>
2021-03-11 19:45:53 +0100

    Update

--- README.md
+++ README.md
@@ -3,7 +3,25 @@
 
 Media as overlay per global shortcut.
 
-
 ## Setup
 
 Using https://www.electronforge.io/
+
+## Shortcuts
+
+The default shortcut is `Control+S+C`, if you want to show "Thanks" you have to press `Control + S + C + 2`. This application is designed to be used with a Streamdeck and as I don't want to interfere with other global shortcuts I decided to prefix them with SC :)
+
+Currently, the following Gifs are included:
+* `1` Party hard
+* `2` Thanks
+* `3` Hello
+* `4` Confetti
+* `5` See you soon Dwayne
+* `6` See you Spongebob
+  
+  If you have a special gif which takes more or less time than the default duration (`DEFAULT_DISPLAY_TIME`) you can set a `displayTime` for each media element.
+
+## Todos
+
+- [ ] Add configuration screen: Add Media with shortcuts and duration, Change default duration, Change base shortcut
+- [ ] Read file dimensions and adapt window for each media file
--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, globalShortcut, remote } = require('electron')
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+const MEDIA_DIR = 'media/'
+
+const DEFAULT_DISPLAY_TIME = 2000
+
+const BASE_SHORTCUT = 'Control+S+C+'
+
+const MEDIA = [
+  {
+    file: 'party_hard.gif',
+    shortcut: '1',
+    //displayTime: 2000
+  },
+  {
+    file: 'thanks.gif',
+    shortcut: '2'
+  },
+  {
+    file: 'hello.webp',
+    shortcut: '3'
+  },
+  {
+    file: 'confetti.gif',
+    shortcut: '4'
+  },
+  {
+    file: 'see_you_soon_dwayne.gif',
+    shortcut: '5'
+  },
+  {
+    file: 'see_you_soon_spongebob.gif',
+    shortcut: '6'
+  }
+]
+
+let window = null
+
+function createWindow() {
+  window = new BrowserWindow({
+    width: 500,
+    height: 300,
+    transparent: true,
+    frame: false
+  })
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-  });
+app.whenReady().then( () => {
+  createWindow()
+  MEDIA.forEach(gif => {
+    globalShortcut.register(BASE_SHORTCUT + gif.shortcut, async () => {
+      console.log(gif.file, BASE_SHORTCUT + gif.shortcut);
+      await window.loadFile(MEDIA_DIR + gif.file);
+      window.show();
+      setTimeout(async () => {
+        await window.loadFile(MEDIA_DIR + 'placeholder.svg');
+        window.minimize();
+      }, gif.displayTime || DEFAULT_DISPLAY_TIME);
+    })
+  })
+})
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit();
+    app.quit()
   }
-});
+})
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll()
+})
